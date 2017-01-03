2bdd581c46d556f32405c1427e9331f1dbd334a1
diok22 <Dionysis.k22@gmail.com>
2017-01-03 22:23:32 +0200

    button and label from code

--- Third Project/Third Project/ViewController.swift
+++ Third Project/Third Project/ViewController.swift
@@ -8,28 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    let items:[[String]] = [
-        ["A1", "A2", "A3"],
-        ["B1", "B2"],
-        ["C1"]
-    ]
+class ViewController: UIViewController {
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label:UILabel = UILabel(frame: CGRect(x: 20, y: 40, width: 300, height: 30))
+        label.text = "from code"
+        view.addSubview(label)
+        
+        let button:UIButton = UIButton(frame: CGRect(x: 20, y: 100, width: 150, height: 40))
+        button.setTitle("Code button", for: .normal)
+        button.backgroundColor = UIColor.darkGray
+        button.addTarget(self , action: #selector(didClick), for: .touchUpInside)
+        view.addSubview(button)
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return items.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return items[component].count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return items[component][row]
+    func didClick(btn:UIButton) {
+        btn.setTitle("Clicked!", for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
